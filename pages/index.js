7365abeae2394856b4908fdb0ee498fc66847a56
mathesond2 <mathesond2@gmail.com>
2022-06-26 02:10:20 -0400

    wip

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,12 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import {
   Container,
+  Box,
+  Heading,
+  Text,
+  ListItem,
+  UnorderedList,
+  Link,
 } from "@chakra-ui/react";
 import Footer from "../components/footer";
 import IPFSStorage from "../components/nftStorage";
@@ -27,12 +33,38 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <HeadMetadata />
-      <main className={styles.main}>
-        <Container maxW="container.xl">
-          <h1>content here</h1>
+      <main>
+      <Container maxW="container.xl">
+          <Box mb={8}>
+            <Heading as='h1'mb={4}>Blockstories</Heading>
+            <Text fontSize='xl'>
+              The blockchain's footnote tool. Create  stories that appear as timelines annotating a series of transactions with your comments. Alternate stories from other writers are overlayed on top of yours.
+            </Text>
+            <Text fontSize='xl' mt={4}>
+              Expose a hack, research a wallet or track the life of an NFT.
+            </Text>
+          </Box>
+          <Heading as='h2' size='xl'>Recent &amp; Popular Stories</Heading>
+          <UnorderedList className='unordered-list'>
+            <ListItem className='unordered-list-item'>
+              <Link href="#">
+                The Most Traded Cryptopunk
+              </Link>
+            </ListItem>
+            <ListItem className='unordered-list-item'>
+              <Link href="#">
+                Terra’s Last Minute Trades | 3 Stories
+              </Link>
+            </ListItem>
+            <ListItem className='unordered-list-item'>
+              <Link href="#">
+                Joris’s NFT Journey
+              </Link>
+            </ListItem>
+          </UnorderedList>
         </Container>
       </main>
-      <IPFSStorage nftData={nftData} />
+      {/* <IPFSStorage nftData={nftData} /> */}
       <Footer />
     </div>
   );
